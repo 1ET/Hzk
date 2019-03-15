7aebcc852b4b7ed3429b44ef1a1440e8ba0dfc81
LH <1374052652@qq.com>
2019-03-16 00:51:36 +0800

    '登录功能完成'

---
+++ src/components/login/login.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { Flex, WhiteSpace, NavBar, Button, List, InputItem, Toast } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css'
+import './login.css'
+import axios from '../../http'
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            uname: '',
+            pwd: ''
+        }
+    }
+    changeState = (k, v) => {
+        this.setState({
+            [k]: v
+        })
+    }
+    handleLogin = async () => {
+        const { history } = this.props
+        const users = this.state
+        const res = await axios.post(`users/login`, users)
+        const { data, meta } = res.data
+        if (meta.status === 200) {
+            history.push('/')
+        } else {
+            Toast.fail(meta.msg, 1);
+        }
+    }
+    render() {
+        return (
+            <Flex direction="column" justify="center">
+
+                <Flex.Item className="navbar">
+                    <NavBar mode="dark">登录</NavBar>
+                </Flex.Item>
+                <WhiteSpace size="sm" />
+                <Flex.Item>
+                    <List>
+                        <InputItem
+                            value={this.state.uname}
+                            onChange={(v) => { this.changeState('uname', v) }}
+                        >账号</InputItem>
+                        <InputItem
+                            value={this.state.pwd}
+                            onChange={(v) => { this.changeState('pwd', v) }}
+                        >密码</InputItem>
+                        <Button type="primary" style={{ marginTop: '4px' }} onClick={this.handleLogin}>登录</Button>
+                    </List>
+                </Flex.Item>
+            </Flex>
+        )
+    }
+}
+
+export default Login
